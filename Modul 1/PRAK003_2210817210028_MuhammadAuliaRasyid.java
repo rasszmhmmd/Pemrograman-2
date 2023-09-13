47a84e8e1f3373e0caf978e4736c3ee050a08fd5
rasszmhmmd <115336316+rasszmhmmd@users.noreply.github.com>
2023-09-13 18:32:02 +0800

    Add files via upload

---
+++ Modul 1/PRAK003_2210817210028_MuhammadAuliaRasyid.java
@@ -0,0 +1,11 @@
+public class PRAK003_2210817210028_MuhammadAuliaRasyid {
+    public static void main(String[] args) {
+        System.out.println("##     ##  ####   ");
+        System.out.println("###   ###  ##  ## ");
+        System.out.println("## # # ##  ##   ##");
+        System.out.println("##  #  ##  ##   ##");
+        System.out.println("##     ##  ##   ##");
+        System.out.println("##     ##  ##  ## ");
+        System.out.println("##     ##  ####   ");
+              }
+}
---
+++ Modul 1/PRAK004_2210817210028_MuhammadAuliaRasyid.java
@@ -0,0 +1,9 @@
+public class PRAK004_2210817210028_MuhammadAuliaRasyid {
+    public static void main(String[] args) {
+        String nama = "Muhammad Aulia Rasyid";
+        int umur = 19 ;
+         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
+         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
+         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
+          }
+}

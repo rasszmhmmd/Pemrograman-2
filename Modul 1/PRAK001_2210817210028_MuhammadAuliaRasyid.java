8efdfbb0347f3372651bc84dfe3c9325362daa9e
rasszmhmmd <115336316+rasszmhmmd@users.noreply.github.com>
2023-10-03 07:11:27 +0800

    Delete Modul 1 directory

--- Modul 1/PRAK001_2210817210028_MuhammadAuliaRasyid.java
+++
@@ -1,7 +0,0 @@
-public class PRAK001_2210817210028_MuhammadAuliaRasyid{
-    public static void main(String[] args) {
-     System.out.println("Selamat Pagi, Muhammad Aulia Rasyid");
-     System.out.println("Selamat Siang, Muhammad Aulia Rasyid");
-     System.out.println("Selamat Malam, Muhammad Aulia Rasyid");
-      }
-}
--- Modul 1/PRAK004_2210817210028_MuhammadAuliaRasyid.java
+++
@@ -1,9 +0,0 @@
-public class PRAK004_2210817210028_MuhammadAuliaRasyid {
-    public static void main(String[] args) {
-        String nama = "Muhammad Aulia Rasyid";
-        int umur = 19 ;
-         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
-         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
-         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
-          }
-}
